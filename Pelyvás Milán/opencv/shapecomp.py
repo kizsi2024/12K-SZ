c1a2fdd7997c038149d52bbc7bf39ecc8c09bb15
Pelyvás Milán <pelyvas.milan20k@egressy.info>
2025-02-18 12:31:44 +0100

    shapefind

---
+++ Pelyvás Milán/opencv/shapecomp.py
@@ -0,0 +1,85 @@
+import cv2
+import numpy as np
+
+def stackImages(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(rows):
+            for y in range(cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2:
+                    imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2:
+                imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        ver = hor
+    return ver
+
+def getContours(img, imgContour):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 500:
+            cv2.drawContours(imgContour, [cnt], -1, (0, 255, 0), 3)
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+            x, y, w, h = cv2.boundingRect(approx)
+            objCol = len(approx)
+
+            if objCol == 3:
+                objectType = "Triangle"
+            elif objCol == 4:
+                aspRatio = w / float(h)
+                if 0.95 < aspRatio < 1.05:
+                    objectType = "Square"
+                else:
+                    objectType = "Rectangle"
+            elif objCol == 5:
+                objectType = "Pentagon"
+            elif objCol > 5:
+                objectType = "Circle"
+            else:
+                objectType = "None"
+
+            cv2.rectangle(imgContour, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            cv2.putText(imgContour, objectType, (x + (w // 2) - 30, y + (h // 2)), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
+
+# Read the image
+img = cv2.imread("Resources/shapes.png")
+imgContour = img.copy()
+
+# Preprocess the image
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray, (7, 7), 1)
+imgCanny = cv2.Canny(imgBlur, 50, 50)
+
+# Get contours and draw them
+getContours(imgCanny, imgContour)
+
+# Stack images for display
+imgStack = stackImages(0.6, ([img, imgGray, imgBlur],
+                             [imgCanny, imgContour, np.zeros_like(img)]))
+
+# Display the result
+cv2.imshow("Stacked Images", imgStack)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
